f4a2f7ee0a971a3fc065fb17c66ab6d545955f34
Raj3460 <sarkarrajkumar3460@gmail.com>
2026-01-10 00:12:35 +0600

    add nodemailer

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,20 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+// nodemailer transporter setup (for demonstration purposes, using Ethereal Email)
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.APP_USER,
+    pass: process.env.APP_PASS,
+  },
+});
+
+console.log(process.env.APP_PASS, "**************", process.env.APP_USER);
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -27,5 +41,19 @@ export const auth = betterAuth({
   },
   emailAndPassword: {
     enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      console.log(`Send verification email to***************8`);
+      const info = await transporter.sendMail({
+        from: '"PRISMA MAKE RAJ" <prismafromraj@tmail.com>',
+        to: "sarkarrajkumar3460@gmail.com",
+        subject: "Hello ✔",
+        text: "Hello world?", // Plain-text version of the message
+        html: "<b>Hello world?</b>", // HTML version of the message
+      });
+    },
   },
 });
